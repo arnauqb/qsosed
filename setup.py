86a5fe20f8c2a97cb0699ef76730dea9367ce6b7
Arnau <arnau.quera@gmail.com>
2019-02-26 17:07:51 +0000

    modified setup

--- setup.py
+++ setup.py
@@ -10,13 +10,19 @@ with open('LICENSE') as f:
 
 setup(
     name='pyagn',
-    version='0.1.0',
+    version='0.2.0',
     description='PyAGN: A Python code about AGNs.',
     long_description=readme,
     author='Arnau Quera-Bofarull',
     author_email='arnau.quera-bofarull@durham.ac.uk',
     url='https://github.com/arnauq/pyagn',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires = ['numpy',
+                        'astropy',
+                        'scipy',
+                        'matplotlib',
+                        'memoized_property']
+
 )
 
